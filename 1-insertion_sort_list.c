e532bdfce53d75221cb0865c4078ac15db8b033e
Israelshecktar <iamshecktar1996@.com>
2023-11-20 13:00:15 +0100

    a function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - Sorts a doubly linked list of integers in
+ * ascending order
  * @list: The list to be sorted
  */
 void insertion_sort_list(listint_t **list)
